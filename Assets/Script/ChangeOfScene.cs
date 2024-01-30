1c8eca10d0c9d10a55fe8bfc4407acf52b9db23a
Cristian Arraya <arrayacristian75@gmail.com>
2024-01-30 00:29:05 -0300

    Mi primer commit, ya se agrego la escena del menu

---
+++ Assets/Script/ChangeOfScene.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeOfScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ChangeOfSceneMenu();
+        }
+    }
+    public void ChangeOfSceneMenu()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
---
+++ Assets/Script/SwitchState.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchState : MonoBehaviour
+{
+    public GameObject targetObject;
+    public float changeStateTime;
+
+    private void Start()
+    {
+        InvokeRepeating("ChangeObjectState", 0f, changeStateTime);
+    }
+    void ChangeObjectState()
+    {
+        targetObject.SetActive(!targetObject.activeInHierarchy);
+    }
+}
